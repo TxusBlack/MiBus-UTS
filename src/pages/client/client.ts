5acb4fa75fc8bb0e98fdebeea34ef6653645f938
Diego Moreno <iam@diegomoreno.co>
2019-11-24 18:14:16 -0500

    Get all buses

--- src/pages/client/client.ts
+++ src/pages/client/client.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as firebase from 'firebase';
 
 @IonicPage()
 @Component({
@@ -11,11 +12,27 @@ export class ClientPage {
   // Obtener todos los puntos y mostrarlos
   public lat = 6.5516271;
   public lng = -73.1337569;
+  public buses = [];
+  public iconBus = {
+    url: '../../assets/imgs/pin-bus.png',
+    scaledSize: {height: 40, width: 40},
+  };
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams
   ) {
+  }
+
+  async ionViewWillEnter() {
+    console.log('Will Enter');
+    const snapshot = await firebase.firestore().collection('usuarios').get();
+    if (!snapshot.empty) {
+      snapshot.forEach(buses => {
+        this.buses.push(buses.data());
+        console.log(this.buses);
+      });
+    }
   }
 
 }
