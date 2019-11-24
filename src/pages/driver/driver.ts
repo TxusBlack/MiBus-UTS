b6c7cad7beba36ac8a68710db89d6130d59d323b
Diego Moreno <iam@diegomoreno.co>
2019-11-24 17:12:32 -0500

    Driver finished

--- src/pages/driver/driver.ts
+++ src/pages/driver/driver.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform, ToastController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Subscription } from 'rxjs';
+import { Device } from '@ionic-native/device';
+
+import * as firebase from 'firebase';
 
 @IonicPage()
 @Component({
@@ -15,25 +18,55 @@ export class DriverPage {
     lat: null,
     lng: null
   }
-
   private watch: Subscription;
+  public uuid;
+  public iconBus = {
+    url: '../../assets/imgs/pin-bus.png',
+    scaledSize: {height: 40, width: 40},
+  };
+
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private geolocation: Geolocation
+    private geolocation: Geolocation,
+    private platform: Platform,
+    private device: Device,
+    private toastCtrl: ToastController
   ) {
   }
 
+  async saveGps(lat, lng) {
+    return await firebase.firestore().collection('usuarios').doc(this.uuid).update({
+      lat: lat,
+      lng: lng
+    });
+  }
+
   watchGps() {
-    this.watch = this.geolocation.watchPosition().subscribe((gps) => {
+    this.watch = this.geolocation.watchPosition().subscribe(async (gps) => {
       this.geo.lat = gps.coords.latitude;
       this.geo.lng = gps.coords.longitude;
+      this.saveGps(this.geo.lat,  this.geo.lng).then(() => {
+        this.toastCtrl.create({
+          message: 'Se enviaron los datos al servidor',
+          duration: 1000
+        }).present();
+      });
     });
   }
 
   ionViewWillEnter() {
    this.watchGps();
+  }
+
+  ionViewDidLoad() {
+    if (this.platform.is('cordova')) {
+      this.uuid = this.device.uuid;
+      console.log('uuid', this.uuid);
+    } else {
+      this.uuid = '1234567890';
+    }
   }
 
   ionViewDidLeave() {
