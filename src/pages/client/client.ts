1bcea678ea10b00c358b495bd272543232ccbb27
Diego Moreno <iam@diegomoreno.co>
2019-11-24 19:04:28 -0500

    Client finished

--- src/pages/client/client.ts
+++ src/pages/client/client.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import * as firebase from 'firebase';
+import { Subscription } from 'rxjs';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @IonicPage()
 @Component({
@@ -10,22 +12,38 @@ import * as firebase from 'firebase';
 export class ClientPage {
 
   // Obtener todos los puntos y mostrarlos
-  public lat = 6.5516271;
-  public lng = -73.1337569;
   public buses = [];
   public iconBus = {
     url: '../../assets/imgs/pin-bus.png',
     scaledSize: {height: 40, width: 40},
   };
 
+  private watch: Subscription
+  public geo = {
+    lat: null,
+    lng: null
+  };
+  public iconPerson = {
+    url: '../../assets/imgs/person.png',
+    scaledSize: {height: 30, width: 30},
+  };
+
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams
+    public navParams: NavParams,
+    private geolocation: Geolocation
   ) {
   }
 
-  async ionViewWillEnter() {
-    console.log('Will Enter');
+  watchGps() {
+    this.watch = this.geolocation.watchPosition().subscribe(async (gps) => {
+      this.geo.lat = gps.coords.latitude;
+      this.geo.lng = gps.coords.longitude;
+      console.log('GPS');
+    });
+  }
+
+  async getData() {
     const snapshot = await firebase.firestore().collection('usuarios').get();
     if (!snapshot.empty) {
       snapshot.forEach(buses => {
@@ -33,6 +51,15 @@ export class ClientPage {
         console.log(this.buses);
       });
     }
+  }
+
+  ionViewWillEnter() {
+    this.getData();
+    this.watchGps();
+  }
+
+  ionViewDidLeave() {
+    this.watch.unsubscribe();
   }
 
 }
